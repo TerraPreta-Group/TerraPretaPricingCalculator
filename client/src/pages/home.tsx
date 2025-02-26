bfbef2962208efbaaa1aebefd02e2e1888189e1b
lee144 <39301533-lee144@users.noreply.replit.com>
2025-02-26 04:59:19 +0000

    Update: Changed button style to use outline variant.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/a39b4c57-d45d-4d0a-b564-4aedb5e449a0/c995e504-f43a-49b4-8a58-a9013fe10ed7.jpg

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -404,12 +404,12 @@ export default function Home() {
           {/* Action Buttons */}
           <div className="flex gap-4 justify-center pt-4">
             <Link href={`/contact?type=order&product=${requiredProduct}&cost=${totalCost}&acres=${acres.toFixed(2)}`}>
-              <Button className="border-2 border-[#003703] hover:bg-[#003703]/10 text-[#003703] text-lg py-6 px-8 shadow-lg">
+              <Button variant="outline" className="border-2 border-[#003703] hover:bg-[#003703]/10 text-[#003703] text-lg py-6 px-8 shadow-lg">
                 Next Step
               </Button>
             </Link>
             <Link href="/contact?type=call">
-              <Button className="border-2 border-[#011028] hover:bg-[#011028]/10 text-[#011028] text-lg py-6 px-8 shadow-lg">
+              <Button variant="outline" className="border-2 border-[#011028] hover:bg-[#011028]/10 text-[#011028] text-lg py-6 px-8 shadow-lg">
                 Questions?
               </Button>
             </Link>
